2eae8f363536a181000de21eb962ed70e43e39a7
medvedevmju-1 <bearmusic2017@gmail.com>
2023-11-21 17:58:29 +0300

    Коммит

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import io
 import random
 import sys
 
@@ -6,10 +7,58 @@ from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
+class Interface:
+    def __init__(self):
+        self.ui = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>800</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <layout class="QGridLayout" name="gridLayout">
+    <item row="0" column="0">
+     <widget class="QPushButton" name="pushButton">
+      <property name="text">
+       <string>Кнопка</string>
+      </property>
+     </widget>
+    </item>
+   </layout>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>800</width>
+     <height>19</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+"""
+
+
 class Suprematism(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi('UI.ui', self)
+        ui = Interface()
+        ui = io.StringIO(ui.ui)
+        loadUi(ui, self)
         self.circle = False
         self.square = False
         self.triangle = False
@@ -39,9 +88,9 @@ class Suprematism(QMainWindow):
     def draw(self):
         x, y = random.randint(0, 800), random.randint(0, 600)
         random_number = random.randint(20, 100)
-        rgb_r = 255
-        rgb_g = 255
-        rgb_b = 0
+        rgb_r = random.randint(0, 255)
+        rgb_g = random.randint(0, 255)
+        rgb_b = random.randint(0, 255)
         color = QColor(rgb_r, rgb_g, rgb_b)
         self.qp.setBrush(color)
 
